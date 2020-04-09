73095584626e06eb530df47b8424e237fd92d57c
Davide Gessa (dakk) <gessadavide@gmail.com>
2020-04-09 10:22:03 +0200

    fix starter script

--- start_bitcoind.sh
+++ start_bitcoind.sh
@@ -1,2 +1,8 @@
-/root/bitcoin-0.19.1/bin/bitcoind -regtest -txindex -server -rpcuser=test -rpcpassword=test \
-	-limitancestorsize=404 -limitdescendantcount=100 -limitancestorcount=100 -debug -printtoconsole
+#!/bin/bash
+set -exm
+
+/root/bitcoin-0.19.1/bin/bitcoind -rpcport=18433 -regtest -txindex -server -rpcuser=test -rpcpassword=test -limitancestorsize=404 -limitdescendantcount=100 -limitancestorcount=100 -debug -printtoconsole &
+
+sleep 5
+
+fg
