7c5fce37752790cc9daaefbb16b55bd94c2fb241
Davide Gessa (dakk) <gessadavide@gmail.com>
2020-04-09 11:13:27 +0200

    fix rpc port

--- start_bitcoind.sh
+++ start_bitcoind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -exm
-/opt/bitcoin-0.19.1/bin/bitcoind -rpcport=18433 -regtest -txindex -server -rpcuser=test -rpcpassword=test -limitancestorsize=404 -limitdescendantcount=100 -limitancestorcount=100 -debug -printtoconsole &
+/opt/bitcoin-0.19.1/bin/bitcoind -rpcport=18443 -regtest -txindex -server -rpcuser=test -rpcpassword=test -limitancestorsize=404 -limitdescendantcount=100 -limitancestorcount=100 -debug -printtoconsole &
 sleep 5
 fg
