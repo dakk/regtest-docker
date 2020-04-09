433d9eaf05ae9859bbb547f962c7699e26b16ea1
Davide Gessa (dakk) <gessadavide@gmail.com>
2020-04-09 10:10:53 +0200

    fix bitcoind fullpath

--- start_bitcoind.sh
+++ start_bitcoind.sh
@@ -1,2 +1,2 @@
-./bitcoin-0.19.1/bin/bitcoind -regtest -txindex -server -rpcuser=test -rpcpassword=test \
+/root/bitcoin-0.19.1/bin/bitcoind -regtest -txindex -server -rpcuser=test -rpcpassword=test \
 	-limitancestorsize=404 -limitdescendantcount=100 -limitancestorcount=100 -debug -printtoconsole
